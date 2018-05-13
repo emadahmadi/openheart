e7fa2447cbf53cfeacc5a3aab66537de242e920e
Emad <emadahmadi1991@gmail.com>
2018-05-13 11:52:17 +0000

    Push It

--- wp-content/themes/tora/home.php
+++ wp-content/themes/tora/home.php
@@ -10,7 +10,6 @@ get_header(); ?>
 
 	<div id="primary" class="content-area <?php echo esc_attr(tora_blog_layout()); ?>">
 		<main id="main" class="site-main" role="main">
-
 		<?php if ( have_posts() ) : ?>
 
 			<div class="posts-layout">
--- wp-content/themes/tora/page-templates/page_fullwidth.php
+++ wp-content/themes/tora/page-templates/page_fullwidth.php
@@ -10,11 +10,11 @@ Template Name: Full width
 
 	<div id="primary" class="content-area fullwidth">
 		<main id="main" class="site-main" role="main">
-
+		
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
+	
 				<?php
 
 					if ( comments_open() || '0' != get_comments_number() ) :
